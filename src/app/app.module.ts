b1ca298a826ab12b6c2ce2c2b38bcadd3cfd1969
Efrat-Fatal <efrat709@gmail.com>
2025-09-16 19:49:21 +0300

    task 4

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,9 +1,10 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
+import { StudentsListComponent } from './students_list/students_list.component';
 
 @NgModule({
-  declarations: [AppComponent],
+  declarations: [AppComponent, StudentsListComponent],
   imports: [BrowserModule],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/students_list/students_list.component.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+import { Student } from '../models/students';
+
+@Component({
+  selector: 'app-students-list',
+  templateUrl: './students-list.component.html',
+  styleUrls: ['./students-list.component.css']
+})
+export class StudentsListComponent {
+  students: Student[] = [
+    new Student(1, 'אורי', 'כהן', '050-1234567', 'ירושלים', 90, true),
+    new Student(2, 'רות', 'לוי', '050-7654321', 'בני ברק', 85, true),
+    new Student(3, 'דוד', 'מזרחי', '050-3333333', 'תל אביב', 70, false),
+  ];
+}
