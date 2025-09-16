94df1bc9b0c45d9221068116d59311b96aedd710
Efrat-Fatal <efrat709@gmail.com>
2025-09-16 19:39:01 +0300

    second task - create basic componant

---
+++ src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  template: `
+    <h1>ברוכים הבאים!</h1>
+    <p>קומפוננטה ראשית מוכנה 🎉</p>
+  `
+})
+export class AppComponent {}
